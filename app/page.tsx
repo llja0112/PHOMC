8a14732123e72584ebdedde70bc35f0bbd947726
Li Lianjie Anthony <llja0112@gmail.com>
2024-04-07 07:10:59 +0000

    Add cards to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import mainPic from '@/public/main.jpg';
 import mainMobilePic from '@/public/main_mobile.jpg';
 import organisersPic from '@/public/organisers.jpg';
 import '@/styles/home.css';
+import {FaHandshake, FaRocket, FaHouseMedical} from 'react-icons/fa6';
 
 export default function () {
 
@@ -34,7 +35,7 @@ export default function () {
       </div>
       <div id="content" className="pt-3">
         <div className="container">
-          <div className="card mb-5">
+          <div className="card mb-3">
             <div className="row g-0">
               <div className="card-body">
                 <h5 className="card-title text-center fs-1">
@@ -47,9 +48,42 @@ export default function () {
                 <p className="card-text">
                   This year, under the inspiring theme <strong className="text-danger">"Frontiers in Health"</strong>, the conference is organized by the College of Public Health and Occupational Physicians of the Academy of Medicine, Singapore. As an annual gathering, the conference prides itself on assembling a diverse and distinguished panel of public health professionals, practitioners, students, academics, researchers, policy-makers, and stakeholders from across the globe. This multidisciplinary event serves as a vital platform for exchanging cutting-edge insights, fostering intellectual dialogue, and exploring the latest innovations in the field of public health and occupational medicine.
                 </p>
-                <p className="card-text">
-                  The 2024 edition of the conference promises an enriching agenda, delving into crucial issues within population health, re-emerging infectious diseases, challenges of an ageing population, health of vulnerable groups, workplace safety and health, and aviation medicine. With a spotlight on innovations in health, the conference aims to showcase groundbreaking advancements and technological breakthroughs that are setting new paradigms in healthcare delivery and disease prevention. Furthermore, it will highlight significant opportunities for collaboration within the healthcare ecosystem, emphasising the importance of unified efforts in tackling current and future public health challenges.
-                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row mb-5">
+            <div className="col-md-4 mb-3">
+              <div className="card">
+                <div className="card-body text-center">
+                  <FaHouseMedical className="fs-1 text-primary mb-3"/>
+                  <h4>Crucial Health Issues</h4>
+                  <p>
+                    PHOMC 2024 promises an enriching agenda, delving into crucial issues within population health, re-emerging infectious diseases, challenges of an ageing population, health of vulnerable groups, workplace safety and health, and aviation medicine. 
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 mb-3">
+              <div className="card">
+                <div className="card-body text-center">
+                  <FaRocket className="fs-1 text-primary mb-3"/>
+                  <h4>Innovation</h4>
+                  <p>
+                    With a spotlight on innovations in health, the conference aims to showcase groundbreaking advancements and technological breakthroughs that are setting new paradigms in healthcare delivery and disease prevention. 
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 mb-3">
+              <div className="card">
+                <div className="card-body text-center">
+                  <FaHandshake className="fs-1 text-primary mb-3"/>
+                  <h4>Collaboration</h4>
+                  <p>
+                    Finally, the conference will highlight significant opportunities for collaboration within the healthcare ecosystem, emphasising the importance of unified efforts in tackling current and future public health challenges.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
