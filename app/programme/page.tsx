0573cb1b4037c6505a0ac9d06298a44bd23a3ad5
Li Lianjie Anthony <llja0112@gmail.com>
2024-05-08 13:59:41 +0000

    Update conference programme

--- app/programme/page.tsx
+++ app/programme/page.tsx
@@ -15,24 +15,58 @@ export default function Page () {
             </thead>
             <tbody>
               <tr className="table-success">
-                <th scope="row">08:30am</th>
+                <th scope="row">08:00am</th>
                 <td colSpan={3}>Registration</td>
               </tr>
               <tr>
-                <th scope="row">09:00am</th>
-                <td colSpan={3}>Opening Ceremony</td>
-              </tr>
-              <tr className="table-primary">
-                <th scope="row">09:50am</th>
-                <td colSpan={3}>Opening Keynote</td>
+                <th scope="row">08:30am</th>
+                <td colSpan={3}>Arrival of Guest of Honour</td>
               </tr>
               <tr>
-                <th scope="row">10:15am</th>
-                <td colSpan={3}>Morning Teabreak & Viewing of Posters</td>
+                <th scope="row">08:35am</th>
+                <td colSpan={3}>
+                  <p>
+                    <b>Welcome Address</b><br />
+                    Prof Gan Wee Hoe, <em>Council President, College of Public Health and Occupational Medicine</em>
+                  </p>
+                  <p>
+                    <b>Welcome Remarks</b> <br /> 
+                    Dr Wong Chiang Yin, <em>Master, Academy of Medicine</em>
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">09:00am</th>
+                <td colSpan={3}>
+                  <b>Opening Address</b><br />
+                  Ms Lai Wai Lin, <em>Permanent Secretary (Designate), Ministry of Health</em>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">09:10am</th>
+                <td colSpan={3}>College Award Ceremony</td>
               </tr>
               <tr className="table-primary">
-                <th scope="row">10:45am</th>
-                <td colSpan={3}>Plenary 1: Designing Societies for Health</td>
+                <th scope="row">09:15am</th>
+                <td colSpan={3}>
+                  <p>
+                    <b>Keynote 1: Building Healthier Societies - Lessons from the social and behavioural sciences for Healthier SG</b><br />
+                    Prof David Chan, <em>Professor of Psychology and Director of Beh Sciences Initiative, Singapore Management University</em>
+                  </p>
+                  <p>
+                    <b>Session Chairperson:</b> Dr Lim John Wah
+                  </p>
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">10:00am</th>
+                <td colSpan={3}>Morning Teabreak & Viewing of Posters</td>
+              </tr>
+              <tr className="table-info">
+                <th scope="row">10:30am</th>
+                <td colSpan={3}>
+                  <b>Plenary 1: Frontiers in Primary & Community Health</b>
+                </td>
               </tr>
               <tr>
                 <th scope="row">12:30am</th>
@@ -40,24 +74,40 @@ export default function Page () {
               </tr>
               <tr className="table-primary">
                 <th scope="row">01:15pm</th>
-                <td colSpan={3}>Plenary 2: Frontiers in Primary Care</td>
+                <td colSpan={3}>
+                  <p>
+                    <b>Keynote 2: Health of vulnerable populations</b><br />
+                    Prof Michelle A. Williams, <em>Former Dean, School of Public Health, Havard University</em>
+                  </p>
+                  <p>
+                    <b>Session Chairperson:</b> Dr Lim Huai Yang
+                  </p>
+                </td>
+              </tr>
+              <tr className="table-info">
+                <th scope="row">02:00pm</th>
+                <td colSpan={3}><b>Plenary 2: Longevity Medicine – Pushing the Boundaries for Healthy Aging</b></td>
               </tr>
               <tr>
                 <th scope="row">03:00pm</th>
                 <td colSpan={3}>Afternoon Teabreak & Viewing of Posters</td>
               </tr>
-              <tr className="table-success">
-                <th scope="row">03:30pm</th>
-                <td colSpan={3}>Concurrent tracks</td>
+              <tr className="table-info">
+                <th scope="row">03:15pm</th>
+                <td colSpan={3}><b>Plenary 3: Age is Just a Number - Building Frailty Resilient Communities</b></td>
+              </tr>
+              <tr className="table-warning">
+                <th scope="row">04:30pm</th>
+                <td colSpan={3}><b>Concurrent sessions 1</b></td>
               </tr>
               <tr>
                 <th scope="row"></th>
-                <td className="table-light"><strong>Track 1:</strong> Longevity - Pushing the Boundaries for Healthy Aging</td>
-                <td className="table-light"><strong>Track 2:</strong> Advances in Healthcare for the Vulnerable </td>
-                <td className="table-light"><strong>Track 3:</strong> Free Paper</td>
+                <td className="table-light" colSpan={1}><strong>Track 1:</strong> Advances in Healthcare for the Vulnerable</td>
+                <td className="table-light" colSpan={1}><strong>Track 2:</strong> Frontiers in Aviation Medicine</td>
+                <td className="table-light" colSpan={1}><strong>Track 3:</strong> Frontiers in Digital Health</td>
               </tr>
               <tr className="table-success">
-                <th scope="row">05:15pm</th>
+                <th scope="row">06:15pm</th>
                 <td colSpan={3}>End of Day 1</td>
               </tr>
             </tbody>
@@ -79,41 +129,54 @@ export default function Page () {
               </tr>
               <tr className="table-primary">
                 <th scope="row">09:00am</th>
-                <td colSpan={3}>Plenary 3: Game Changing Technology in Population Health</td>
+                <td colSpan={3}>
+                  <p>
+                    <b>Keynote 3</b><br />
+                    Dr Kenji Saito, <em>Chairman, American College of Occupational & Environmental Medicine</em>
+                  </p>
+                </td>
               </tr>
               <tr>
-                <th scope="row">10:30am</th>
+                <th scope="row">09:30am</th>
                 <td colSpan={3}>Morning Teabreak & Viewing of Posters</td>
               </tr>
-              <tr className="table-primary">
-                <th scope="row">10:45am</th>
-                <td colSpan={3}>Plenary 3: Game Changing Technology in Population Health Panel Discussion</td>
-              </tr>
-              <tr>
-                <th scope="row">11:30am</th>
-                <td colSpan={3}>Lunch & Viewing of Posters</td>
-              </tr>
-              <tr className="table-success">
-                <th scope="row">12:30pm</th>
-                <td colSpan={3}>Concurrent tracks</td>
+              <tr className="table-warning">
+                <th scope="row">10:00am</th>
+                <td colSpan={3}>
+                  <b>Concurrent sessions 2</b>
+                </td>
               </tr>
               <tr>
                 <th scope="row"></th>
-                <td className="table-light"><strong>Track 4:</strong> Advances in Occupational Health</td>
-                <td className="table-light"><strong>Track 5:</strong> Digital Health</td>
+                <td className="table-light"><strong>Track 4:</strong> Frontiers in Occupational Health</td>
+                <td className="table-light"><strong>Track 5:</strong> Game Changing Technology in Health</td>
                 <td className="table-light"><strong>Track 6:</strong> Free Paper</td>
               </tr>
               <tr>
-                <th scope="row">03:00pm</th>
+                <th scope="row">11:45am</th>
+                <td colSpan={3}>Lunch & Viewing of Posters</td>
+              </tr>
+              
+              <tr className="table-primary">
+                <th scope="row">12:30pm</th>
+                <td colSpan={3}><b>Plenary 4: Frontiers in Infectious Disease Prevention and Pandemic Response</b></td>
+              </tr>
+              
+              <tr>
+                <th scope="row">02:30pm</th>
                 <td colSpan={3}>Afternoon Teabreak & Viewing of Posters</td>
               </tr>
               <tr className="table-primary">
                 <th scope="row">03:30pm</th>
-                <td colSpan={3}>Plenary 4: Frontiers in Healthcare Policy Innovation</td>
+                <td colSpan={3}><b>Plenary 5: Frontiers in Healthcare Policy Innovation</b></td>
               </tr>
               <tr>
                 <th scope="row">04:15pm</th>
-                <td colSpan={3}>Awards Presentation & Closing Ceremony</td>
+                <td colSpan={3}>Awards Presentation</td>
+              </tr>
+              <tr>
+                <th scope="row">05:00pm</th>
+                <td colSpan={3}>Closing Ceremony</td>
               </tr>
               <tr className="table-success">
                 <th scope="row">05:15pm</th>
